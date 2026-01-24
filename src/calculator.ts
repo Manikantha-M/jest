1955d60ba217165cb463bbf41ed06defbf5bed4e
Manikantha M <manikantha.m@neutrinos.co>
2026-01-24 10:01:31 +0300

    calculator calss

---
+++ src/calculator.ts
@@ -0,0 +1,19 @@
+// class tests
+
+export default class Calculator {
+    value:number;
+    constructor(value:number){
+        this.value = 0;
+    };
+    add(num:number):number{
+        this.value += num;
+        return this.value;
+    };
+    subtract(num:number):number{
+        this.value -= num;
+        return this.value;
+    };
+    isPositive():boolean{
+        return this.value > 0;
+    }
+}
