023bc905961d8f654015bd1335e38e2d446b1bf9
Manikantha M <manikantha.m@neutrinos.co>
2026-01-24 09:56:22 +0300

    utils test complted

--- src/__tests__/utils.spec.ts
+++ src/__tests__/utils.spec.ts
@@ -10,5 +10,20 @@ describe('Utility Functions', () => {
     it('returns true for even number',()=>{
         expect(isEven(2)).toBe(true);
         expect(isEven(3)).not.toBe(true);
-    })
+    });
+    it('creates user object with correct properties', () => {
+        const user = createUser('john doe', 3);
+        expect(user).toEqual({name:'john doe', age:3});
+        expect(user).not.toEqual({name:'susan doe', age:25});
+    });
+    it('create JWT Token', async()=>{
+        const token = await createJWTtoken();
+        expect(token).toBe('jwt_token');
+    });
+    it('calculate average correcly', ()=>{
+        expect(calculateAverage([2,4,6,8])).toBe(5);
+    });
+    it('calculate avg for empty number arr', () => {
+        expect(calculateAverage([])).toBe(0)
+    });
 })
